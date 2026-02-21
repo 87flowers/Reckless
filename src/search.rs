ccbafbd4a63e4f34f0c9282487c824d79fe4b849
87 <dev@87flowers.com>
2026-02-21 14:55:33 +0100

    Bench: 4060307

--- src/search.rs
+++ src/search.rs
@@ -813,6 +813,10 @@ fn search<NODE: NodeType>(
                 reduction += 128;
             }
 
+            if !NODE::PV && td.stack[ply - 1].reduction < reduction - 512 {
+                reduction -= 128;
+            }
+
             let reduced_depth = (new_depth - reduction / 1024).clamp(1, new_depth + 1) + 2 * NODE::PV as i32;
 
             td.stack[ply].reduction = reduction;
@@ -875,6 +879,10 @@ fn search<NODE: NodeType>(
 
             if !NODE::PV && td.stack[ply - 1].reduction > reduction + 512 {
                 reduction += 128;
+            }
+
+            if !NODE::PV && td.stack[ply - 1].reduction < reduction - 512 {
+                reduction -= 128;
             }
 
             let reduced_depth = new_depth - (reduction >= 3072) as i32 - (reduction >= 5687 && new_depth >= 3) as i32;
