7679e690ccf0628faf191d1edfa5267f6c8862f5
87 <dev@87flowers.com>
2026-02-26 15:39:27 +0100

    Bench: 3675105

--- src/search.rs
+++ src/search.rs
@@ -497,9 +497,14 @@ fn search<NODE: NodeType>(
     }
 
     let improving = improvement > 0;
+    let no_opponent_threats = (td.board.threats() & td.board.us()).is_empty() && !td.board.in_check();
 
     // Razoring
-    if !NODE::PV && !in_check && estimated_score < alpha - 299 - 252 * depth * depth && alpha < 2048 {
+    if !NODE::PV
+        && !in_check
+        && estimated_score < alpha - 299 - 252 * depth * depth - 100 * no_opponent_threats as i32
+        && alpha < 2048
+    {
         return qsearch::<NonPV>(td, alpha, beta, ply);
     }
 
@@ -512,7 +517,7 @@ fn search<NODE: NodeType>(
             >= beta + 1125 * depth * depth / 128 + 26 * depth - (77 * improving as i32)
                 + 519 * correction_value.abs() / 1024
                 + 32 * (depth == 1) as i32
-                - 64 * ((td.board.threats() & td.board.us()).is_empty() && !td.board.in_check()) as i32
+                - 64 * no_opponent_threats as i32
         && !is_loss(beta)
         && !is_win(estimated_score)
     {
