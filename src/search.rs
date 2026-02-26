90660e7323f89095bf25e6f90e7e6ed6dbcc99cb
87 <dev@87flowers.com>
2026-02-26 03:38:08 +0100

    Bench: 3779005

--- src/search.rs
+++ src/search.rs
@@ -1042,6 +1042,7 @@ fn search<NODE: NodeType>(
             factor += 113 * (pcm_move == td.stack[ply - 1].tt_move) as i32;
             factor += 156 * (!in_check && best_score <= eval.min(raw_eval) - 96) as i32;
             factor += 317 * (is_valid(td.stack[ply - 1].eval) && best_score <= -td.stack[ply - 1].eval - 120) as i32;
+            factor += 100 * (td.stack[ply - 1].reduction > 256) as i32;
 
             let scaled_bonus = factor * (153 * depth - 34).min(2474) / 128;
 
