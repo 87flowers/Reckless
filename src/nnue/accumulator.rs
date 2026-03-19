556b58218098af945be587c2410f0933fd3d261b
87 <dev@87flowers.com>
2026-03-19 17:41:07 +0100

    Bench: 2795637

--- src/nnue/accumulator.rs
+++ src/nnue/accumulator.rs
@@ -1,8 +1,4 @@
-use super::{Aligned, L1_SIZE, PARAMETERS, simd};
-use crate::{
-    nnue::INPUT_BUCKETS,
-    types::{Bitboard, Color, PieceType},
-};
+use crate::nnue::INPUT_BUCKETS;
 
 pub mod psq;
 pub mod threats;
@@ -12,22 +8,5 @@ pub use threats::ThreatAccumulator;
 
 #[derive(Clone, Default)]
 pub struct AccumulatorCache {
-    entries: Box<[[[CacheEntry; INPUT_BUCKETS]; 2]; 2]>,
-}
-
-#[derive(Clone)]
-pub struct CacheEntry {
-    values: Aligned<[i16; L1_SIZE]>,
-    pieces: [Bitboard; PieceType::NUM],
-    colors: [Bitboard; Color::NUM],
-}
-
-impl Default for CacheEntry {
-    fn default() -> Self {
-        Self {
-            values: PARAMETERS.ft_biases.clone(),
-            pieces: [Bitboard::default(); PieceType::NUM],
-            colors: [Bitboard::default(); Color::NUM],
-        }
-    }
+    entries: Box<[[[psq::CacheEntry; INPUT_BUCKETS]; 2]; 2]>,
 }
