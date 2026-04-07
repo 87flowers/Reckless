7626712eee35e59e7c49a894059e564e135614d8
87 <dev@87flowers.com>
2026-04-08 00:37:40 +0200

    Bench: 2594185

--- src/movepick.rs
+++ src/movepick.rs
@@ -174,7 +174,7 @@ impl MovePicker {
 
                 entry.score = 16 * captured.value()
                     + td.noisy_history.get(threats, td.board.moved_piece(mv), mv.to(), captured)
-                    + 4000
+                    + 7000
                         * (discovery.contains(mv.from()) && !ray_pass(their_king, mv.from()).contains(mv.to())) as i32;
             }
         }
