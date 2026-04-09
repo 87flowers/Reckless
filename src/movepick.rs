bc2659d220170908d5a2e3f3ac7a8c7d1284daf1
87 <dev@87flowers.com>
2026-04-10 00:26:15 +0200

    Bench: 2975370

--- src/movepick.rs
+++ src/movepick.rs
@@ -247,7 +247,7 @@ impl MovePicker {
                 + 6158 * offense[pt].contains(mv.to()) as i32
                 + 5000 * (pt == PieceType::Rook && king_ring_ortho.contains(mv.to())) as i32
                 - 4000 * wall_pawns.contains(mv.from()) as i32
-                - 3000 * (td.board.checking_squares(pt).contains(mv.to()) && offense[pt].contains(mv.to())) as i32;
+                + 3000 * (td.board.checking_squares(pt).contains(mv.to()) && offense[pt].contains(mv.to())) as i32;
         }
     }
 }
