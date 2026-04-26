77ae84d352084142b0ff81ae75627c16f0f83545
87 <dev@87flowers.com>
2026-04-26 21:58:33 +0200

    Bench: 2549046

--- src/search.rs
+++ src/search.rs
@@ -590,7 +590,7 @@ fn search<NODE: NodeType>(
     {
         let mut move_picker = MovePicker::new_probcut(probcut_beta - eval);
 
-        while let Some(mv) = move_picker.next::<NODE>(td, true, ply) {
+        while let Some(mv) = move_picker.next::<NODE>(td, false, ply) {
             if move_picker.stage() == Stage::BadNoisy {
                 break;
             }
