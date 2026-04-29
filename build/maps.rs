a3c7dacab08d9e68b81c6a4c1c85d5d4fd363faf
87 <dev@87flowers.com>
2026-04-29 22:58:04 +0200

    Bench: 2982858

--- build/maps.rs
+++ build/maps.rs
@@ -1,5 +1,8 @@
 use crate::{attacks::*, magics::*};
 
+const ROOK_DIRECTIONS: [(i8, i8); 4] = [(1, 0), (-1, 0), (0, 1), (0, -1)];
+const BISHOP_DIRECTIONS: [(i8, i8); 4] = [(1, 1), (1, -1), (-1, 1), (-1, -1)];
+
 pub fn generate_king_map() -> [u64; 64] {
     generate_map(king_attacks)
 }
@@ -26,11 +29,11 @@ pub fn generate_black_pawn_map() -> [u64; 64] {
 }
 
 pub fn generate_rook_map() -> Vec<u64> {
-    generate_sliding_map(ROOK_MAP_SIZE, &ROOK_MAGICS, &[(1, 0), (-1, 0), (0, 1), (0, -1)])
+    generate_sliding_map(ROOK_MAP_SIZE, &ROOK_MAGICS, &ROOK_DIRECTIONS)
 }
 
 pub fn generate_bishop_map() -> Vec<u64> {
-    generate_sliding_map(BISHOP_MAP_SIZE, &BISHOP_MAGICS, &[(1, 1), (1, -1), (-1, 1), (-1, -1)])
+    generate_sliding_map(BISHOP_MAP_SIZE, &BISHOP_MAGICS, &BISHOP_DIRECTIONS)
 }
 
 pub fn generate_diagonal_table() -> Vec<u64> {
@@ -45,6 +48,44 @@ pub fn generate_anti_diagonal_table() -> Vec<u64> {
     let mut map = vec![0; 64];
     for square in 0..64 {
         map[square as usize] = sliding_attacks(square, 0, &[(1, -1), (-1, 1)]);
+    }
+    map
+}
+
+pub fn generate_between_map() -> [[u64; 64]; 64] {
+    let mut map = [[0; 64]; 64];
+    for a in 0..64 {
+        for b in 0..64 {
+            let a_bb = 1 << a;
+            let b_bb = 1 << b;
+
+            map[a as usize][b as usize] = if sliding_attacks(a, 0, &ROOK_DIRECTIONS) & b_bb != 0 {
+                sliding_attacks(a, b_bb, &ROOK_DIRECTIONS) & sliding_attacks(b, a_bb, &ROOK_DIRECTIONS)
+            } else if sliding_attacks(a, 0, &BISHOP_DIRECTIONS) & b_bb != 0 {
+                sliding_attacks(a, b_bb, &BISHOP_DIRECTIONS) & sliding_attacks(b, a_bb, &BISHOP_DIRECTIONS)
+            } else {
+                0
+            };
+        }
+    }
+    map
+}
+
+pub fn generate_ray_pass_map() -> [[u64; 64]; 64] {
+    let mut map = [[0; 64]; 64];
+    for a in 0..64 {
+        for b in 0..64 {
+            let a_bb = 1 << a;
+            let b_bb = 1 << b;
+
+            map[a as usize][b as usize] = if sliding_attacks(a, 0, &ROOK_DIRECTIONS) & b_bb != 0 {
+                b_bb | (sliding_attacks(a, 0, &ROOK_DIRECTIONS) & sliding_attacks(b, a_bb, &ROOK_DIRECTIONS))
+            } else if sliding_attacks(a, 0, &BISHOP_DIRECTIONS) & b_bb != 0 {
+                b_bb | (sliding_attacks(a, 0, &BISHOP_DIRECTIONS) & sliding_attacks(b, a_bb, &BISHOP_DIRECTIONS))
+            } else {
+                0
+            };
+        }
     }
     map
 }
